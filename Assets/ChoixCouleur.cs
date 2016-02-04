cf18a2b70c6982028dd17b71f711512308e4b2bf
SASUBI <v.lucain@student.isartdigital.com>
2016-02-03 23:40:16 -0500

    Menu et couleur Set

---
+++ Assets/ChoixCouleur.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class ChoixCouleur : MonoBehaviour
+{
+    public CouleurRune Couleur;
+    SpriteRenderer BrickRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        BrickRenderer = GetComponent<SpriteRenderer>();
+        //CouleurRune
+        if (Couleur == CouleurRune.Rouge)
+        {
+            BrickRenderer.material.color = GestionCouleur.Rouge;
+        }
+        if (Couleur == CouleurRune.Violet)
+        {
+            BrickRenderer.material.color = GestionCouleur.Violet;
+        }
+        if (Couleur == CouleurRune.Bleu)
+        {
+            BrickRenderer.material.color = GestionCouleur.Bleu;
+        }
+        if (Couleur == CouleurRune.Turquoise)
+        {
+            BrickRenderer.material.color = GestionCouleur.Turquoise;
+        }
+        if (Couleur == CouleurRune.Vert)
+        {
+            BrickRenderer.material.color = GestionCouleur.Vert;
+        }
+        if (Couleur == CouleurRune.Orange)
+        {
+            BrickRenderer.material.color = GestionCouleur.Orange;
+        }
+    }
+
+}
---
+++ Assets/GestionCouleur.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum CouleurRune
+{
+    Rouge,Violet,Bleu,Turquoise,Vert,Orange
+}
+
+public static class GestionCouleur
+{
+
+    public static Color Rouge = new Color(0.659F, 0.306F, 0.306F);
+    public static Color Violet = new Color(0.639F, 0.404F, 0.635F);
+    public static Color Bleu = new Color(0.412F, 0.404F, 0.627F);
+    public static Color Turquoise = new Color(0.404F, 0.561F, 0.62F);
+    public static Color Vert = new Color(0.404F, 0.608F, 0.451F);
+    public static Color Orange = new Color(0.667F, 0.475F, 0.224F);
+}
